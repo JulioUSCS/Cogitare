f3d55f675adb53ffebb75b7708b2e47ec86d4a77
Julio Francisco Bernardino <juliofranciscobernardino@gmail.com>
2025-09-09 17:56:22 -0300

    Update idosoModel.js

--- model/idosoModel.js
+++ model/idosoModel.js
@@ -4,7 +4,6 @@ import pool from '../config/db.js';
 const Idoso = {
   async listar() {
     const [rows] = await pool.query('SELECT * FROM idoso');
-    console.log(rows);
     return rows;
 
   },
